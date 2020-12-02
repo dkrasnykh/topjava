093c0d49a45c177e5be6fea843639276155e1770
dkrasnykh <krasnykh.daria@gmail.com>
2020-12-02 20:47:19 +0300

    prepare to HW08

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,39 @@
+var ctx;
+
+// $(document).ready(function () {
+$(function () {
+    // https://stackoverflow.com/a/5064235/548473
+    ctx = {
+        ajaxUrl: "admin/meals/",
+        datatableApi: $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Update",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "asc"
+                ]
+            ]
+        })
+    };
+    makeEditable();
+});
